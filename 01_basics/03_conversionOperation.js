6f0f25cd3572f8f0a50eb0877f778a3453de4642
GitForTiger <apratimdubey.nitrkl@gmail.com>
2026-05-17 17:04:10 +0000

    Conversion Of Datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,19 @@
+// let score = "99abe"
+// console.log(typeof score)
+// // console.log(typeof(score)) // same as above
+// let scoreInNumber = Number(score)
+// console.log(typeof scoreInNumber)
+// console.log(scoreInNumber)
+
+// "33" => 33
+// "33abe" => NaN => type iss still integer
+// true <=> 1 ; false <=> 0
+
+// let isLoggedIn = "apratim"
+// console.log(Boolean(isLoggedIn))
+// "apratim" => true ; "" => false or 0
+
+let someNumber = 10
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
