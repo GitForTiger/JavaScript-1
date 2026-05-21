35743f5d4e9c3f32b7ed9c1b4802e89dcd4d1325
GitForTiger <apratimdubey.nitrkl@gmail.com>
2026-05-21 15:19:59 +0000

    Continuation with conversion operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -15,8 +15,41 @@
 
 let SomeNumber = 10
 let stringNumber = String(SomeNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
 
 let stringg="Am I present?"
-console.log(stringg+"apple")
+// console.log(stringg+"apple")
+
+// ******************** Operations ********************
+let value=3
+let negValue=-value
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2**3)
+// console.log(2%3)
+
+let str1="hello"
+let str2="Apratim"
+let str3=str1+str2
+// console.log("2"+"1"); //21
+// console.log("2"+1)    //21
+// console.log(2+"1")    //21
+
+// console.log("1"+2+2)     //122
+// console.log(1+2+"2")     //32
+//Basically the type present at first as the argument is considered.
+//But this type of practice is NOT considered good.
+
+console.log(true) //true
+console.log(+true) //1
+console.log(+"") //0
+
+let num1,num2,num3
+console.log(num1) //undefined
+num1=num2=num3=2+2
+
